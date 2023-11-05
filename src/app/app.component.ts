f936f54abeeb86114604592a03493adf263a011d
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-05 18:51:16 +0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from "./auth/auth.service";
 import { Observable } from 'rxjs';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 
 })
+
 export class AppComponent implements OnInit {
 
   //learning viewchild
