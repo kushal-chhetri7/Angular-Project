ccad0fb046c45f4d64dbd6e95cbd546405921a6d
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-06 10:44:46 +0600

    Update dropdown.directive.ts

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -3,12 +3,12 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 @Directive({
   selector: '[appDropdown]'
 })
+
 export class DropdownDirective {
 
-  constructor(private elementRef:ElementRef) { }
+  constructor(private elementRef:ElementRef) {}
 
   @HostListener('click') ClickedDropDown(){
     this.elementRef.nativeElement.classList.toggle('open')
   }
-
 }
