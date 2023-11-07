b00bb0c5c80aa862ed484ed98c06aad0d7bc9058
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-08 00:38:14 +0600

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,7 +11,7 @@ import { ShoppingListService } from './shopping-list.service';
 })
 
 export class ShoppingListComponent implements OnInit, OnDestroy {
-
+ 
   ingredients: Ingredient[];
   private subscription: Subscription;
 
